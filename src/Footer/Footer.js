a966592ed72b29e4f375c0c0b50c8077f33d25dc
mostafizurRahaman <fahim654326@gmail.com>
2022-12-11 22:20:59 +0600

    footer section is done

---
+++ src/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+   return (
+      <div className=' bg-primary  text-2xl font-semibold capitalize mt-20 text-center  text-secondary px-5 py-10'>
+         <p className="">&copy;All rights reserved by Mostafizur Rahaman </p>
+      </div>
+   );
+};
+
+export default Footer;
--- src/Layout/Main/Main.js
+++ src/Layout/Main/Main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Footer from '../../Footer/Footer';
 import Header from '../../Pages/Shared/Header/Header';
 
 
@@ -7,7 +8,8 @@ const Main = () => {
    return (
       <div>
         <Header></Header>       
-         <Outlet></Outlet>      
+         <Outlet></Outlet>
+         <Footer></Footer>      
       </div>
    );
 };
